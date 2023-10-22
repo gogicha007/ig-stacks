187c8fe2987f0ea9e589ee0641ee40de71a845c6
gogicha007 <gogicha@gmail.com>
2023-10-22 22:12:31 +0400

    chor: save data

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,55 +3,12 @@ import Link from "next/link";
 import React from "react";
 import styles from "@/components/navbar/Navbar.module.css";
 import Image from "next/image";
+import data from "@/dictionaries/sidebar.json"
 import { useSelectedLayoutSegment } from "next/navigation";
 
-const links = [
-  {
-    id: 1,
-    icon: "/images/home.svg",
-    title: "მთავარი",
-    url: "/",
-  },
-  {
-    id: 2,
-    icon: "/images/inventory.svg",
-    title: "საწყობი",
-    url: "/inventory",
-  },
-  {
-    id: 3,
-    icon: "/images/suppliers.svg",
-    title: "მომწოდებლები",
-    url: "/suppliers",
-  },
-  {
-    id: 4,
-    icon: "/images/orders.svg",
-    title: "მოთხოვნები",
-    url: "/orders",
-  },
-  {
-    id: 5,
-    icon: "/images/invoice.svg",
-    title: "ინვოისები",
-    url: "/invoices",
-  },
-  {
-    id: 6,
-    icon: "/images/reports.svg",
-    title: "რეპორტები",
-    url: "/reports",
-  },
-  {
-    id: 7,
-    icon: "/images/settings.svg",
-    title: "პარამეტრები",
-    url: "/settings",
-  },
-];
+const links = data
 
 const Navbar = ({ onClick }) => {
-  // onClick = (e) => {console.log(e.target.text)}
   return (
     <div className={styles.container}>
       <div className={styles.links}>
